977b27961127c8ec108f4491169eb74cc36791ea
MartaT10 <martatopolnjak2@gmail.com>
2026-02-02 11:17:51 +0100

    Add files via upload

--- LV16.cpp
+++ LV16.cpp
@@ -3,15 +3,22 @@
 using namespace std;
 
 void a_ocjene();
+void b_matura();
 
 int main()
 {
-    a_ocjene();
+    //a_ocjene();
+    //b_matura();
 
     return 0;
 }
 
 void a_ocjene() {
+    /* ruj lis stu pro sij velj ožu tra svi lip
+        0   5   0   0   0    5   0   0   4   0
+        0   0   0   2   0    0   3   0   0   2
+        0   4   2   0   0    0   5   0   4   0  */
+
     string mjeseci[10];
     int tablica[3][10];
     int petice = 0, ocjene = 0;
@@ -60,4 +67,51 @@ void a_ocjene() {
     }
 
 
+}
+
+void b_matura() {
+    /*      pon uto sri cet pet sub ned
+        MAT 2   1   0.5 0   1.5 0   1
+        HJ  1   2   1   2   0   0   1.5
+        EJ  1   1.5 0   0   1.5 0   0   */
+
+    string dani[10];
+    string predmeti[3];
+    float tablica[3][7];
+    float ukupno = 0, psati = 168, dsati = 168, b1;
+    string predmet = ""; string dan = "";
+
+    for (int i = 0;i < 7;i++) {
+        cin >> dani[i];
+    }
+
+    for (int i = 0;i < 3;i++) {
+        b1 = 0;
+        cin >> predmeti[i];
+        for (int j = 0;j < 7;j++) {
+            cin >> tablica[i][j];
+            ukupno += tablica[i][j];
+            b1 += tablica[i][j];
+        }
+        if (b1 < psati) {
+            predmet = predmeti[i];
+            psati = b1;
+        }
+    }
+
+    for (int i = 0;i < 7;i++) {
+        b1 = 0;
+        for (int j = 0;j < 3;j++) {
+            b1 += tablica[j][i];
+        }
+        if (b1 < dsati) {
+            dan = dani[i];
+            dsati = b1;
+        }
+    }
+
+    cout << "Ukupno je ucio " << ukupno << " sati.\n";
+    cout << "Najmanje je ucio " << predmet << " i to " << psati << " sati.\n";
+    cout << "Najmanje je ucio u " << dan << " i to " << dsati << " sati.\n";
+  
 }
